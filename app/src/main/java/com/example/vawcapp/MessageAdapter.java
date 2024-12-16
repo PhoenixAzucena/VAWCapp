0a3b0cd9b8ca3d5feca80170cc9b5ef6d36cbda7
PhoenixAzucena <phoenixjamir22@gmail.com>
2024-12-16 23:49:49 +0800

    changes

---
+++ app/src/main/java/com/example/vawcapp/MessageAdapter.java
@@ -0,0 +1,48 @@
+package com.example.vawcapp;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.List;
+
+public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.MessageViewHolder> {
+
+    private List<Message> messageList;
+
+    public MessageAdapter(List<Message> messageList) {
+        this.messageList = messageList;
+    }
+
+    @NonNull
+    @Override
+    public MessageViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(android.R.layout.simple_list_item_2, parent, false);
+        return new MessageViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MessageViewHolder holder, int position) {
+        Message message = messageList.get(position);
+        holder.textViewSender.setText(message.getSender());
+        holder.textViewMessage.setText(message.getText());
+    }
+
+    @Override
+    public int getItemCount() {
+        return messageList.size();
+    }
+
+    static class MessageViewHolder extends RecyclerView.ViewHolder {
+        TextView textViewSender;
+        TextView textViewMessage;
+
+        public MessageViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textViewSender = itemView.findViewById(android.R.id.text1);
+            textViewMessage = itemView.findViewById(android.R.id.text2);
+        }
+    }
+}
