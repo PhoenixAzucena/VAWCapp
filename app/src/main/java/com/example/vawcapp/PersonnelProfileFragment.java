351ee61fa517055e80e248bc5be0c0323861f5f6
PhoenixAzucena <phoenixjamir22@gmail.com>
2024-12-18 06:17:13 +0800

    changes

--- app/src/main/java/com/example/vawcapp/PersonnelProfileFragment.java
+++ app/src/main/java/com/example/vawcapp/PersonnelProfileFragment.java
@@ -1,5 +1,6 @@
 package com.example.vawcapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -45,7 +47,7 @@ public class PersonnelProfileFragment extends Fragment {
         fragment.setArguments(args);
         return fragment;
     }
-
+private Button Logout;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -58,7 +60,15 @@ public class PersonnelProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_personnel_profile, container, false);
+     View v =   inflater.inflate(R.layout.fragment_personnel_profile, container, false);
+       Logout = v.findViewById(R.id.logout_button);
+       Logout.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               Intent intent = new Intent(requireContext(), MainActivity.class);
+               startActivity(intent);
+           }
+       });
+        return v;
     }
 }
